0f038a2302a234be2a8753d59bf7a901b42d119a
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-16 11:26:07 +0000

    Add debug opt and refactoring

--- get_stats.sh
+++ get_stats.sh
@@ -8,6 +8,7 @@ DATA_STREAM_FR_HOSPITAL_ETS="https://www.data.gouv.fr/fr/datasets/r/41b9bd2a-b5b
 ELASTIC_URL="changeit"
 ELASTIC_USERNAME="changeit"
 ELASTIC_PASSWORD="changeit"
+IS_DEBUG="false"
 
 error() {
     echo "Error : invalid parameter !" >&2
@@ -19,6 +20,7 @@ usage(){
     echo "Usage: ./livraison.sh [options]"
     echo "-h or --help: print help"
     echo "-a or --all: ingest all data"
+    echo "-d or --debug: enable debug traces"
     echo "--ingest-data : ingest data from www.coronavirus-statistiques.com"
     echo "--ingest-data-fr-hospital : ingest data from france with www.data.gouv.fr"
     echo "--ingest-data-fr-hospital-new : ingest data from france with www.data.gouv.fr, new cases"
@@ -44,108 +46,123 @@ hash_id() {
   echo "${1}"|jq -r "${2}"|base64|tr -d '='
 }
 
-ingest_data() {
-  log_file="covid19.log"
+get_usecase_prefix() {
+  ext=""
+  prefix=""
+  [[ $1 ]] && prefix="gouvfr-"
+  [[ $1 && $1 != "gouvfr" ]] && ext="-${1}"
+  echo "${prefix}covid19${ext}"
+}
 
+get_log_file_name() {
+  echo "$(get_usecase_prefix $1).log"
+}
+
+get_indice_name() {
+  echo "$(get_log_file_name ${1})-$(format_year_month ${2})"
+}
+
+start_log() {
+  date > $(get_log_file_name "${1}")
+}
+
+end_log() {
+  bash -c "echo ''; date" >> $(get_log_file_name "${1}")
+}
+
+push_document() {
+  json="${1}"
+  vdate="${2}"
+  jsonquery="${3}"
+  usecase="${4}"
+  [[ $IS_DEBUG == "true" ]] && set -x
+
+  log_file=$(get_log_file_name "${usecase}")
+  id=$(hash_id "${json}" "${jsonquery}")
+
+  indice_url="${ELASTIC_URL}/$(get_indice_name ${usecase} ${vdate})/_doc/${id}"
+  if [[ $(format $vdate) != "null" ]]; then
+    curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
+  fi
+}
+
+ingest_data() {
   i=0
-  date > "${log_file}"
-  curl "${DATA_STREAM}" 2>>"${log_file}"|while IFS=';' read vdate vcode vplace vcases vdeath vrecover vsource trash; do
+  start_log
+  curl "${DATA_STREAM}" 2>>$(get_log_file_name)|while IFS=';' read vdate vcode vplace vcases vdeath vrecover vsource trash; do
     if [[ $i -gt 0 ]]; then
       json="{\"vdate\":\"$(format $vdate)\",\"vcode\":\"$(format $vcode)\",\"vplace\":\"$(format $vplace)\",\"vcases\":$(format_number $vcases),\"vdeath\":$(format_number $vdeath),\"vrecover\":$(format_number $vrecover),\"vsource\":\"$(format "$vsource")\"}"
-      id=$(hash_id "${json}" '.vdate+.vcode+.vplace+.vsource')
-      indice_url="${ELASTIC_URL}/covid19-$(format_year_month $vdate)/_doc/${id}"
-      if [[ $(format $vdate) != "null" ]]; then
-        curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
-      fi
+      push_document "${json}" "${vdate}" '.vdate+.vcode+.vplace+.vsource'
     fi
     (( i++ ))
   done
-  date >> "${log_file}"
+  end_log
 }
 
 ingest_data_fr_hostpital() {
-  log_file="gouvfr-covid19.log"
-
   i=0
-  date > "${log_file}"
-  curl -L "${DATA_STREAM_FR_HOSPITAL}" 2>>"${log_file}"|while IFS=';' read vplace vgender vdate vcases vrea vrad vdc trash; do
+  usecase="gouvfr"
+  start_log "${usecase}"
+  curl -L "${DATA_STREAM_FR_HOSPITAL}" 2>>$(get_log_file_name "${usecase}")|while IFS=';' read vplace vgender vdate vcases vrea vrad vdc trash; do
     if [[ $i -gt 0 ]]; then
       json="{\"vdate\":\"$(format $vdate)\",\"vgender\":$(format_number $vgender),\"vplace\":\"$(format $vplace)\",\"vcases\":$(format_number $vcases),\"vrea\":$(format_number $vrea),\"vrecover\":$(format_number $vrad),\"vdeath\":$(format_number "$vdc"),\"vsource\":\"www.data.gouv.fr\"}"
-      id=$(hash_id "${json}" '.vdate+(.vgender|tostring)+.vplace+.vsource')
-      indice_url="${ELASTIC_URL}/gouvfr-covid19-$(format_year_month $vdate)/_doc/${id}"
-      if [[ $(format $vdate) != "null" ]]; then
-        curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
-      fi
+      push_document "${json}" "${vdate}" '.vdate+(.vgender|tostring)+.vplace+.vsource' "${usecase}"
     fi
     (( i++ ))
   done
-  date >> "${log_file}"
+  end_log "${usecase}"
 }
 
 ingest_data_fr_hostpital_new() {
-  log_file="gouvfr-new-covid19.log"
-
   i=0
-  date > "${log_file}"
-  curl -L "${DATA_STREAM_FR_HOSPITAL_NEW}" 2>>"${log_file}"|while IFS=';' read vplace vdate vcases vrea vdc vrad trash; do
+  usecase="new"
+  start_log "${usecase}"
+  curl -L "${DATA_STREAM_FR_HOSPITAL_NEW}" 2>>$(get_log_file_name "${usecase}")|while IFS=';' read vplace vdate vcases vrea vdc vrad trash; do
     if [[ $i -gt 0 ]]; then
       json="{\"vdate\":\"$(format $vdate)\",\"vplace\":\"$(format $vplace)\",\"vcases\":$(format_number $vcases),\"vrea\":$(format_number $vrea),\"vrecover\":$(format_number $vrad),\"vdeath\":$(format_number "$vdc"),\"vsource\":\"www.data.gouv.fr\"}"
-      id=$(hash_id "${json}" '.vdate+.vplace+.vsource')
-      indice_url="${ELASTIC_URL}/gouvfr-new-covid19-$(format_year_month $vdate)/_doc/${id}"
-      if [[ $(format $vdate) != "null" ]]; then
-        curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
-      fi
+      push_document "${json}" "${vdate}" '.vdate+.vplace+.vsource' "${usecase}"
     fi
     (( i++ ))
   done
-  date >> "${log_file}"
+  end_log "${usecase}"
 }
 
 ingest_data_fr_hostpital_age() {
-  log_file="gouvfr-age-covid19.log"
-
   i=0
-  date > "${log_file}"
-  curl -L "${DATA_STREAM_FR_HOSPITAL_AGE}" 2>>"${log_file}"|while IFS=';' read vplace vage vdate vcases vrea vrad vdc trash; do
+  usecase="age"
+  start_log "${usecase}"
+  curl -L "${DATA_STREAM_FR_HOSPITAL_AGE}" 2>>$(get_log_file_name "${usecase}")|while IFS=';' read vplace vage vdate vcases vrea vrad vdc trash; do
     if [[ $i -gt 0 ]]; then
       json="{\"vdate\":\"$(format $vdate)\",\"vplace\":\"$(format $vplace)\",\"vage\":$(format_number $vage),\"vcases\":$(format_number $vcases),\"vrea\":$(format_number $vrea),\"vrecover\":$(format_number $vrad),\"vdeath\":$(format_number "$vdc"),\"vsource\":\"www.data.gouv.fr\"}"
-      id=$(hash_id "${json}" '.vdate+(.vage|tostring)+.vplace+.vsource')
-      indice_url="${ELASTIC_URL}/gouvfr-age-covid19-$(format_year_month $vdate)/_doc/${id}"
-      if [[ $(format $vdate) != "null" ]]; then
-        curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
-      fi
+      push_document "${json}" "${vdate}" '.vdate+(.vage|tostring)+.vplace+.vsource' "${usecase}"
     fi
     (( i++ ))
   done
-  date >> "${log_file}"
+  end_log "${usecase}"
 }
 
 ingest_data_fr_hostpital_ets() {
-  log_file="gouvfr-ets-covid19.log"
-
   i=0
-  date > "${log_file}"
-  curl -L "${DATA_STREAM_FR_HOSPITAL_ETS}" 2>>"${log_file}"|while IFS=';' read vplace vdate vcases trash; do
+  usecase="ets"
+  start_log "${usecase}"
+  curl -L "${DATA_STREAM_FR_HOSPITAL_ETS}" 2>>$(get_log_file_name "${usecase}")|while IFS=';' read vplace vdate vcases trash; do
     if [[ $i -gt 0 ]]; then
       json="{\"vdate\":\"$(format $vdate)\",\"vplace\":\"$(format $vplace)\",\"vcases\":$(format_number $vcases),\"vsource\":\"www.data.gouv.fr\"}"
-      id=$(hash_id "${json}" '.vdate+.vplace+.vsource')
-      indice_url="${ELASTIC_URL}/gouvfr-ets-covid19-$(format_year_month $vdate)/_doc/${id}"
-      if [[ $(format $vdate) != "null" ]]; then
-        curl "${indice_url}" -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT -d "${json}" -H "Content-Type: application/json" >> "${log_file}" 2>>"${log_file}"
-      fi
+      push_document "${json}" "${vdate}" '.vdate+.vplace+.vsource' "${usecase}"
     fi
     (( i++ ))
   done
-  date >> "${log_file}"
+  end_log "${usecase}"
 }
 
 [[ $# -lt 1 ]] && error
 
-options=$(getopt -o a,h,s -l help,all,ingest-data,ingest-data-fr-hospital,ingest-data-fr-hospital-new,ingest-data-fr-hospital-age,ingest-data-fr-hospital-ets -- "$@")
+options=$(getopt -o a,h,s,d -l help,debug,all,ingest-data,ingest-data-fr-hospital,ingest-data-fr-hospital-new,ingest-data-fr-hospital-age,ingest-data-fr-hospital-ets -- "$@")
 set -- $options 
 while true; do 
     case "$1" in 
         -h|--help) usage ; shift ;;
+        -d|--debug) IS_DEBUG="true" ; shift ;;
         --ingest-data) ingest_data ; shift ;;
         --ingest-data-fr-hospital) ingest_data_fr_hostpital ; shift ;;
         --ingest-data-fr-hospital-new) ingest_data_fr_hostpital_new ; shift ;;
