c5e0c0fb90413570738e76edd5db7afb7a084653
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-26 20:53:31 +0100

    Deploy on comwork infrastructure

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-REPO_PATH="${PROJECT_HOME}/tekton-task-images/"
+REPO_PATH="${PROJECT_HOME}/covid19/"
 
 cd "${REPO_PATH}" && git pull origin "${GIT_BRANCH}"  || :
 git push github "${GIT_BRANCH}" 
 git push pgitlab "${GIT_BRANCH}" 
-git push pgithub "${GIT_BRANCH}" 
 exit 0
