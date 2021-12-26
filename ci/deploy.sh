3aad97e04b2a96cd3e42b92729c35680da0e6d8f
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-26 20:59:46 +0100

    Deploy on comwork infrastructure

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,4 @@ echo "ELASTIC_USERNAME=${ELASTIC_USERNAME}" >> .env
 echo "ELASTIC_PASSWORD=${ELASTIC_PASSWORD}" >> .env
 
 docker rmi -f "comworkio/covid-stats:latest"
-docker-compose -f docker-compose-x86-min.yml up --force-recreate
+docker-compose -f docker-compose-x86-min.yml up -d --force-recreate
