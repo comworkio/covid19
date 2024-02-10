2f42e9dc6fcdcd3c554370344dbaaf7be8718df0
Idriss Neumann <neumann.idriss@gmail.com>
2024-02-10 10:47:28 +0000

    Update 2 files
    - /ci/mirror.sh
    - /README.md

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,6 +5,4 @@ REPO_PATH="${PROJECT_HOME}/covid19/"
 cd "${REPO_PATH}" && git pull origin "${GIT_BRANCH}"  || :
 git push github "${GIT_BRANCH}" 
 git push pgitlab "${GIT_BRANCH}"
-git push bitbucket "${GIT_BRANCH}"
-git push froggit "${GIT_BRANCH}"
 exit 0
